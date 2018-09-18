8794e2615d1787df7e83f15d06b541783af7b349
sweida <weidaxyy@163.com>
2018-09-18 20:26:47 +0800

    登录api

--- database/migrations/2018_09_18_105026_create_table_userinfos.php
+++ database/migrations/2018_09_18_105026_create_table_userinfos.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTableUsersinfo extends Migration
+class CreateTableUserinfos extends Migration
 {
     /**
      * Run the migrations.
