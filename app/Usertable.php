9d93bf51d10db3a9c504062937b8dadce87b8478
sweida <weidaxyy@163.com>
2018-10-15 02:04:31 +0800

    route::group

--- app/Usertable.php
+++ app/Usertable.php
@@ -69,12 +69,28 @@ class Usertable extends Model
             }                
         }    
 
-        // 所有用户
+        // 每页多少条
+        $limit = rq('limit') ?: 10;
+        // 页码，从第limit条开始
+        $skip = (rq('page') ? rq('page')-1 : 0) * $limit;
+
+        // 按创建时间排序
         $users = $this
             ->orderBy('created_at')
-            ->get();
+            ->limit($limit)
+            ->skip($skip)
+            ->get(['id', 'username', 'avatar_url', 'email'])
+            ->keyBy('id');
 
+        // 查看所有提问，默认15条
         return suc(['data' => $users]);
+
+        // // 所有用户
+        // $users = $this
+        //     ->orderBy('created_at')
+        //     ->get();
+
+        // return suc(['data' => $users]);
     }
 
     // 获取所有用户列表
--- routes/web.php
+++ routes/web.php
@@ -100,10 +100,6 @@ Route::any('api/is_login', function() {
     return user_ins()->is_login();
 });
 
-Route::any('api/userlist', function() {
-    return user_ins()->userlist();
-});
-
 Route::any('api/user/read', function() {
     return user_ins()->read();
 });
@@ -156,30 +152,33 @@ Route::any('api/user/validata_captcha', function() {
 //     return answer_ins()->vote();
 // });
 
-Route::any('api/timeline', 'CommonController@timeline');
+// Route::any('api/timeline', 'CommonController@timeline');
+
+
 
 // article
 // 中间件，管理员权限
-Route::group(['middleware' => ['adminRole']], function () {
-    Route::any('api/article/add', function() {
+Route::group(['prefix' => 'api/article', 'middleware' => ['adminRole']], function () {
+    Route::any('add', function() {
         return article_ins()->add();
     });
-    Route::any('api/article/change', function() {
+    Route::any('change', function() {
         return article_ins()->change();
     });
 
-    Route::any('api/article/remove', function() {
+    Route::any('remove', function() {
         return article_ins()->remove();
     });
 
-    Route::any('api/article/delete', function() {
+    Route::any('delete', function() {
         return article_ins()->reallyDelete();
     });
 
-    Route::any('api/article/restored', function() {
+    Route::any('restored', function() {
         return article_ins()->restored();
     });
 });
+
 
 Route::any('api/article/like', function() {
     return article_ins()->like();
@@ -189,85 +188,95 @@ Route::any('api/article/read', function() {
     return article_ins()->read();
 });
 
-// 标签
-Route::any('api/tag/read', function() {
-    return tag_ins()->read();
-});
-
 // 按月份查询
 Route::any('api/article/times', function() {
     return article_ins()->times();
 });
 
+// 标签
+Route::any('api/tag/read', function() {
+    return tag_ins()->read();
+});
+
 // 网站信息
 Route::any('api/webinfo/add', function() {
     return webinfo_ins()->add();
-});
+})->middleware('adminRole');
 
 // 友情连接
-Route::any('api/link/add', function() {
-    return link_ins()->add();
-});
+Route::group(['prefix' => 'api/link', 'middleware' => ['adminRole']], function () {
+    Route::any('add', function() {
+        return link_ins()->add();
+    });
 
-Route::any('api/link/change', function() {
-    return link_ins()->change();
-});
+    Route::any('change', function() {
+        return link_ins()->change();
+    });
 
-Route::any('api/link/remove', function() {
-    return link_ins()->remove();
-});
+    Route::any('remove', function() {
+        return link_ins()->remove();
+    });
 
-Route::any('api/link/read', function() {
-    return link_ins()->read();
+    Route::any('read', function() {
+        return link_ins()->read();
+    });
 });
 
 // 留言
-Route::any('api/message/add', function() {
-    return message_ins()->add();
+Route::group(['prefix' => 'api/message'], function () {
+    Route::any('add', function() {
+        return message_ins()->add();
+    });
+
+    Route::any('read', function() {
+        return message_ins()->read();
+    });
+
+    Route::any('change', function() {
+        return message_ins()->change();
+    });
+
+    Route::any('remove', function() {
+        return message_ins()->remove();
+    });
 });
 
-Route::any('api/message/read', function() {
-    return message_ins()->read();
-});
-
-Route::any('api/message/change', function() {
-    return message_ins()->change();
-});
-
-Route::any('api/message/remove', function() {
-    return message_ins()->remove();
-});
 
 // 广告
-Route::any('api/ad/add', function() {
-    return ad_ins()->add();
+Route::group(['prefix' => 'api/ad', 'middleware' => ['adminRole']], function () {
+    Route::any('add', function() {
+        return ad_ins()->add();
+    });
+
+    Route::any('change', function() {
+        return ad_ins()->change();
+    });
+
+    Route::any('remove', function() {
+        return ad_ins()->remove();
+    });
+
+    Route::any('read', function() {
+        return ad_ins()->read();
+    });
 });
 
-Route::any('api/ad/change', function() {
-    return ad_ins()->change();
-});
-
-Route::any('api/ad/remove', function() {
-    return ad_ins()->remove();
-});
-
-Route::any('api/ad/read', function() {
-    return ad_ins()->read();
-});
 
 // 评论文章
-Route::any('api/comment/add', function() {
-    return comment_ins()->add();
-});
+Route::group(['prefix' => 'api/comment', 'middleware' => ['adminRole']], function () {
+    Route::any('add', function() {
+        return comment_ins()->add();
+    });
 
-Route::any('api/comment/read', function() {
-    return comment_ins()->read();
-});
+    Route::any('read', function() {
+        return comment_ins()->read();
+    });
 
-Route::any('api/comment/remove', function() {
-    return comment_ins()->remove();
-});
+    Route::any('remove', function() {
+        return comment_ins()->remove();
+    });
 
-Route::any('api/comment/change', function() {
-    return comment_ins()->change();
+    Route::any('change', function() {
+        return comment_ins()->change();
+    });
 });
