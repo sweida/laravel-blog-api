7f4fd94522369e73d7154ede0c08662893b14420
sweida <weidaxyy@163.com>
2018-10-18 13:47:54 +0800

    用户列表数据类型

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,7 +18,8 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\TrustProxies::class,
+        // \App\Http\Middleware\TrustProxies::class,
+        // \App\Http\Middleware\CORS::class, //跨域访问中间件
     ];
 
     /**
